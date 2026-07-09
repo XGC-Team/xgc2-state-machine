82c695f0e15e716363574bb45463e2349705ad16
Lxk_buaa <867768510@qq.com>
2026-07-09 23:44:03 +0800

    Use bionic-compatible version parsing

--- .xgc2/scripts/build_deb.sh
+++ .xgc2/scripts/build_deb.sh
@@ -15,7 +15,7 @@ arch="$(dpkg --print-architecture)"
 multiarch="$(dpkg-architecture -qDEB_HOST_MULTIARCH)"
 
 product_version() {
-  awk -F': *' '/^version:[[:space:]]*/ {print $2; exit}' "${repo_root}/.xgc2/product.yml"
+  sed -n 's/^version:[[:space:]]*//p' "${repo_root}/.xgc2/product.yml" | head -n 1
 }
 
 package_base_version="${PACKAGE_BASE_VERSION:-$(product_version)}"
